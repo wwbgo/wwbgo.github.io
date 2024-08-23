bf7d7534af1b177b677f751534b33c240957a1f3
wwb <wwbgo@qq.com>
2024-08-23 16:40:23 +0800

    new

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,11 @@
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [tailwind()],
+  site: "https://wwbgo.github.io",
+  integrations: [tailwind(), mdx(), sitemap(), icon()],
 });
